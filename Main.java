b9264c60212a9c104d358b50625f44e29ec71404
Acute-Tri-angle <pptran106@student.hbuhsd.edu>
2024-10-28 11:18:21 -0700

    v2

--- Main.java
+++ Main.java
@@ -9,19 +9,40 @@ public class Main
     System.out.println("ratio: input 2 ints, first int is numerator, 2nd int is denominator");
     int num1 = sc.nextInt();
     int num2 = sc.nextInt();
-    ratio(num1, num2);
+    if (num2 == 0){
+      System.out.println("error, you cant divide by 0 bucko");
+    }
+    else if ((num1 / num2 > 1) && (num1 / num2 <= 8)){
+      System.out.println("ratio OK");
+    }
+    else{
+      System.out.println("ratio not OK >:(");
+    }
 
     // TODO: Problem 2
     System.out.println("divisible: input 2 ints, first int is numerator, 2nd int is denominator");
     num1 = sc.nextInt();
     num2 = sc.nextInt();
-    divisible(num1, num2);
+    if (num2 == 0){
+      System.out.println("error, 0 is an invalid input");
+    }
+    else if (num1 % num2 == 0){
+      System.out.println("2nd num is factor of 1st num");
+    }
+    else{
+      System.out.println("2nd num is not factor of 1st num");
+    }
     
     // TODO: Problem 3
     System.out.println("fifties: Enter an integer in the fifties");
     num1 = sc.nextInt();
-    fifties(num1);
-    
+    if ((num1 >=50) && (num1 <= 59)){
+      System.out.println("your number is " + num1);
+    }
+    else{
+      System.out.println("thats not a number in the fifties!!! \nyour number is 55");
+    }
+
     System.out.println("inclass: input dimensions in double of rectangle 1, len then width, same thing with rectangle 2");
     double lr1 = sc.nextDouble();
     double wr1 = sc.nextDouble();
@@ -30,6 +51,7 @@ public class Main
     Rectangle rect1 = new Rectangle(lr1, wr1);
     Rectangle rect2 = new Rectangle(lr2, wr2);
     System.out.println("are the 2 rectangles equal? " + rectEqual(rect1, rect2));
+
 
     sc.close();
   }
@@ -45,35 +67,5 @@ public class Main
     // implement solution here
     return r1.equals(r2);
   }
-  public static void ratio(int x1, int x2){
 
-    if (x2 == 0){
-      System.out.println("error, you cant divide by 0 bucko");
-    }
-    else if ((x1 / x2 > 1) && (x1 / x2 <= 8)){
-      System.out.println("ratio OK");
-    }
-    else{
-      System.out.println("ratio not OK >:(");
-    }
-  }
-  public static void divisible(int x1, int x2){
-    if (x2 == 0){
-      System.out.println("error, 0 is an invalid input");
-    }
-    else if (x1 % x2 == 0){
-      System.out.println("2nd num is factor of 1st num");
-    }
-    else{
-      System.out.println("2nd num is not factor of 1st num");
-    }
-  }
-  public static void fifties(int x1){
-    if ((x1 >=50) && (x1 <= 59)){
-      System.out.println("your number is " + x1);
-    }
-    else{
-      System.out.println("thats not a number in the fifties!!! \nyour number is 55");
-    }
-  }
 }
